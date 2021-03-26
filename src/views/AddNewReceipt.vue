cf492f58ba187eff3a1946aa350914419dd0bd48
Camilla Dilling-Hansen <camilla@dilling.co>
2021-03-26 11:54:16 +0100

    Finished prototype, made ready for build and added XD file

--- src/views/AddNewReceipt.vue
+++ src/views/AddNewReceipt.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <InputWSuggestions/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
 import InputWSuggestions from "@/components/InputWSuggestions";
+
 export default {
   name: 'Home',
   components: {InputWSuggestions}
